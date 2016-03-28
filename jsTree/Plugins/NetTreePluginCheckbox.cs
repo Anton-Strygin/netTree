f717cc6ace6fcd229ed212225db3675e885d38c9
Stanislav Mursaev <stas.mursaev@gmail.com>
2016-03-28 15:46:36 +0300

    jsTree version updated to 3.3.0

--- jsTree/Plugins/NetTreePluginCheckbox.cs
+++ jsTree/Plugins/NetTreePluginCheckbox.cs
@@ -19,7 +19,9 @@ namespace jsTree.Plugins
                 if (!ThreeState) settings.Add("'three_state': false");
                 if (!WholeNode) settings.Add("'whole_node': false");
                 if (!KeepSelectedStyle) settings.Add("'keep_selected_style': false");
-                
+                if (!string.IsNullOrEmpty(Cascade)) settings.Add("'cascade': '" + Cascade  + "'");
+                if (!TieSelection) settings.Add("'tie_selection': false");
+
                 sb.Append(string.Join(", ", settings.ToArray()));
                 sb.Append(" }");
                 return sb.ToString();
@@ -46,6 +48,18 @@ namespace jsTree.Plugins
         /// </summary>
         public bool KeepSelectedStyle { get; set; }
 
+		/// <summary>
+		/// This setting controls how cascading and undetermined nodes are applied.
+		/// If 'up' is in the string - cascading up is enabled, if 'down' is in the string - cascading down is enabled, if 'undetermined' is in the string - undetermined nodes will be used.
+		/// If `three_state` is set to `true` this setting is automatically set to 'up+down+undetermined'. Defaults to ''.
+        /// </summary>
+		public string Cascade { get; set; }
+		
+        /// <summary>
+		/// This setting controls if checkbox are bound to the general tree selection or to an internal array maintained by the checkbox plugin. Defaults to `true`, only set to `false` if you know exactly what you are doing.
+        /// </summary>
+        public bool TieSelection { get; set; }
+
         public NetTreePluginCheckbox()
         {
             //default values
@@ -53,6 +67,8 @@ namespace jsTree.Plugins
             ThreeState = true;
             WholeNode = true;
             KeepSelectedStyle = false;
+            Cascade = "";
+            TieSelection = true;
         }
     }
 }
